7d557b833a6f5ca2f0529c010dca5d100d5efecd
Umeadi, Marlon <umeadi@gmail.com>
2019-09-11 22:20:08 -0500

    added package.json

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,17 @@ var connection = mysql.createConnection({
     database:"burgers_db"
 })
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ppixa67re8qtpiwi',
+        password: 'xic6ao1qtdwr3h8g',
+        database: 'fkky5iorkkt81cvn'
+    });
+};
+
 connection.connect(function(err) {
     if(err)throw err;
     console.log("Connected as id: " + connection.threadid);
